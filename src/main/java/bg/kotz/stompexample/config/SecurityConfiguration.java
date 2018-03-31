8934e3b666e3dc00f59c67ac0471d03dd5a050e5
kotz <kostadinov92@gmail.com>
2018-03-31 18:26:16 +0300

    Simple UserDetailsService implementation

--- src/main/java/bg/kotz/stompexample/config/SecurityConfiguration.java
+++ src/main/java/bg/kotz/stompexample/config/SecurityConfiguration.java
@@ -1,23 +1,49 @@
 package bg.kotz.stompexample.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-
-        httpSecurity.formLogin()
+        httpSecurity.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
                 .and()
-                .logout()
-                .invalidateHttpSession(true);
+                .formLogin()
+                .and()
+                .logout().permitAll().logoutSuccessUrl("/login").invalidateHttpSession(true);
+
+
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
 
         httpSecurity.headers().frameOptions().disable();
         httpSecurity.csrf().disable();
     }
+
 
 }
