a2cc65d7b4c9e71a634e2f82b86d38a248d45da8
kotz <kostadinov92@gmail.com>
2018-03-31 19:52:21 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+1. run the application with: ./mvnw spring-boot:run
+2. navigate to localhost:8080
+3. the default username and password are admin:admin
+
+
--- src/main/java/bg/kotz/stompexample/Application.java
+++ src/main/java/bg/kotz/stompexample/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StompExampleApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StompExampleApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
